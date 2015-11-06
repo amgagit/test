0b99fd2d0ad481705033d349e1c397d2220dde3c
Amgaa <monkhoo2011@gmail.com>
2015-11-06 11:54:58 +0800

    some base files are modified

--- app/app.js
+++ app/app.js
@@ -2,10 +2,12 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import trimHelper from './helpers/trim';
 
 var App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
+Ember.Hanglebars.registerBoundHelper('trim', trimHelper);
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
